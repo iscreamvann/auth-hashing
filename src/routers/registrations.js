213bd686cb1a2d8cab8e88ee86a1a6847e872d6f
Kyle Vann <kyle.vann1996@icloud.com>
2024-07-11 12:18:54 +0100

    Complete

--- src/routers/registrations.js
+++ src/routers/registrations.js
@@ -4,16 +4,42 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const prisma = require('../utils/prisma.js')
+const saltRounds = 10;
 
 router.post('/', async (req, res) => {
     // Get the username and password from request body
+    const {username, password} = req.body;
+
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    console.log("salt: ", salt, password, req.body)
+
+    if(!username || !password){
+        res.json(400).json({error: "must contain password and username"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+    let createdUser = {}
+    try{
+    createdUser = await prisma.user.create({
+        data: {
+            username,
+            password: hashedPassword
+        },
+    });
+    }
+    catch(er){
+        res.json({"error" :"error adding user"})
+    }
+
+
     
     // Hash the password: https://github.com/kelektiv/node.bcrypt.js#with-promises
     
     // Save the user using the prisma user model, setting their password to the hashed version
     
     // Respond back to the client with the created users username and id
-    res.status(201).json({ user: undefined })
+    res.status(201).json({ user: { id: createdUser?.id, username: createdUser?.username }  })
 });
 
 module.exports = router;
--- src/routers/sessions.js
+++ src/routers/sessions.js
@@ -10,6 +10,32 @@ router.post('/', async (req, res) => {
     // Get the username and password from the request body
 
     // Check that a user with that username exists in the database
+    console.log("username", username)
+    try {
+        const user = await prisma.user.findUniqueOrThrow({
+            where: { username: username },
+        });
+        const compare = await bcrypt.compare(password, user.password)
+        console.log("user", user, password, user.password, compare)
+        if (!user) {
+          return res.status(404).json({ error: 'User not found' });
+        }
+        if(compare !== true){
+            return res.status(400).json({error: "Password incorrect"})
+        }
+        if(compare === true){
+            const token = jwt.sign({username: username}, process.env.JWT_SECRET);
+            return res.status(200).json({jwt: token})
+        }
+        res.status(200).json({ user: user });
+      } catch (error) {
+        if (error instanceof NotFoundError) {
+          res.status(404).json({ error: error.message });
+        } else {
+          res.status(500).json({ error: error.message });
+        }
+      }
+
     // Use bcrypt to check that the provided password matches the hashed password on the user
     // If either of these checks fail, respond with a 401 "Invalid username or password" error
 
